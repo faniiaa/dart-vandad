34736ef38eebc577f51b3fa61fc5e1d225fd4704
faniiaa <103335724+faniiaa@users.noreply.github.com>
2025-06-02 22:07:46 +0200

    class

---
+++ bin/test18.class.dart
@@ -0,0 +1,20 @@
+import 'test3.dart';
+
+void main(List<String> arg){
+  final person = Person();
+  print(person.fullName);
+  print(person.firstName);
+  print(person.lastName);
+}
+
+
+class Person{
+  late String fullName = _getFullName();
+  late String firstName = fullName.split(' ').first;
+  late String lastName = fullName.split(' ').last;
+
+  String _getFullName(){
+    print('_getFullName is called');
+    return 'Fafa Arm';
+  }  
+}
